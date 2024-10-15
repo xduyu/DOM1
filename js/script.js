eac0a85a95c776ca9dc5d3db0196d00a0d36876b
art <147410114+xduyu@users.noreply.github.com>
2024-10-15 18:09:15 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// getElementById()
+
+const p1 = document.getElementById("first")
+
+console.log(p1)
+
+
+
+//getElementsByClassName
+
+const p2a3 = document.getElementsByClassName("3")
+console.log(p2a3)
+
+
+//getElementByTagName
+
+
+const elemntsTag = document.getElementsByTagName("p")
+console.log(elemntsTag)
+
+
+// 
+
+const p12 = document.querySelector("ele")
+
+console.log(p12)
+
+const p13 = document.querySelectorAll("ele")
+
+console.log(p13)
+
